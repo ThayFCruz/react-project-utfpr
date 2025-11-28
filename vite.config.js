d2f878d37e267030439396417f67e4012e16b096
ThayFCruz <37883374+ThayFCruz@users.noreply.github.com>
2025-11-28 18:01:50 -0300

    Update vite.config.js - fix setup tests file name

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/test/setupTests.js'
+    setupFiles: './src/test/SetupTests.js'
   }
 })
